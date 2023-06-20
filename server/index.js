b4f8f3df665f231cf0f516012c6a3fc7c48a641a
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-06-20 21:25:42 +0200

    routes setup

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
+dotenv.config();
 
 import connectDB from "./mongodb/connect.js";
-
-dotenv.config();
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
 
 app.get("/", async (req, res) => {
   res.status(200).json({
---
+++ server/routes/postRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import { v2 as cloudinary } from "cloudinary";
+
+import PostModel from "../mongodb/models/postModel.js";
+
+dotenv.config();
+
+const router = express.Router();
