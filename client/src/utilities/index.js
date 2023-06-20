458e47b27923f8bf642e0d22a25a88a37ede9062
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-06-20 16:27:06 +0200

    adding FormField component

--- client/src/utilities/index.js
+++ client/src/utilities/index.js
@@ -1,9 +1,9 @@
-import { suprieseMePrompts } from "../constants";
+import { surpriseMePrompts } from "../constants";
 
 export function getRandomPrompt(prompt) {
-  const randomIndex = Math.floor(Math.random() * suprieseMePrompts.length);
+  const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
 
-  const randomPrompt = suprieseMePrompts[randomIndex];
+  const randomPrompt = surpriseMePrompts[randomIndex];
 
   if (randomPrompt === prompt) return getRandomPrompt(prompt);
 
