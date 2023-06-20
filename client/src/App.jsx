86767740f2711a6488044fcf574374ee08359e19
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-06-20 14:46:02 +0200

    initial folder and file structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+
+// assets
+import { icon } from "./assets";
+
+// pages
+import { Home, CreatePost } from "./pages";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#efebf4]">
+          <Link to="/">
+            <img src={icon} alt="logo" className="w-20 object-contain" />
+            <h1 className="my-2 text-center font-semibold ">Dall-E 2.0</h1>
+          </Link>
+
+          <Link
+            to="/create-post"
+            className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md"
+          >
+            Create
+          </Link>
+        </header>
+        <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create-post" element={<CreatePost />} />
+            <Route />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
