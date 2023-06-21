c2ef2b240d89a784823c6ea1d65d1025d64c0000
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-06-21 09:29:13 +0200

    fetchPosts in Home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,22 +2,44 @@ import React, { useState, useEffect } from "react";
 
 import { Loader, Card, FormField } from "../components";
 
+const RenderCards = ({ data, title }) => {
+  if (data?.length > 0) {
+    return data.map((post) => <Card key={post._id} {...post} />);
+  }
+
+  return (
+    <h2 className="mt-5 font-bold text-[#6469ff] text-xl uppercase">{title}</h2>
+  );
+};
+
 const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState([null]);
   const [searchText, setSearchText] = useState("");
 
-  const RenderCards = ({ data, title }) => {
-    if (data?.length > 0) {
-      return data.map((post) => <Card key={post._id} {...post} />);
-    }
+  useEffect(() => {
+    const fetchPost = async () => {
+      setLoading(true);
 
-    return (
-      <h2 className="mt-5 font-bold text-[#6469ff] text-xl uppercase">
-        {title}
-      </h2>
-    );
-  };
+      try {
+        const response = await fetch("http://localhost:8080/api/v1/post", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        if (response.ok) {
+          const result = await response.json();
+          setAllPosts(result.data.reverse());
+        }
+      } catch (error) {
+        alert(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchPost();
+  }, []);
 
   return (
     <section className="max-w-7xl mx-auto">
@@ -54,7 +76,7 @@ const Home = () => {
                 {searchText ? (
                   <RenderCards data={[]} title="No search results" />
                 ) : (
-                  <RenderCards data={[]} title="No posts found" />
+                  <RenderCards data={allPosts} title="No posts found" />
                 )}
               </div>
             </>
