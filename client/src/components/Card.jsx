45556d897a9ebb56f35d8456122a9f11e8e7b14e
Bojan Stanimirovski <bojan-stanimirovski097@hotmail.com>
2023-06-21 10:19:45 +0200

    adding Card component

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -14,6 +14,31 @@ const Card = ({ _id, name, prompt, photo }) => {
         alt={prompt}
         className="w-full h-auto object-cover rounded-xl"
       />
+      <div className="group-hover:flex flex-col max-h-[94.5%] hidden absolute bottom-0 left-0 right-0 bg-[#10131f] m-2 p-4 rounded-md">
+        <p className="text-white text-sm overflow-y-hidden prompt">{prompt}</p>
+
+        <div className="mt-5 flex justify-between items-center gap-2">
+          <div className="flex items-center gap-2">
+            <div className="w-7 h-7 rounded-full object-cover bg-green-700 text-white flex justify-center items-center text-sm capitalize font-bold">
+              {name[0]}
+            </div>
+            <p className="text-white text-sm capitalize">{name}</p>
+          </div>
+
+          {/* Download button */}
+          <button
+            type="button"
+            onClick={() => downloadImage(_id, photo)}
+            className="outline-none bg-transparent border-none"
+          >
+            <img
+              src={download}
+              alt="download"
+              className="h-6 w-6 object-contain invert"
+            />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
